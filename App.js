c58501947175aaa2c738621711d180246e8cec22
dodoheeee <hiyoun96@naver.com>
2023-11-30 11:16:08 +0900

    feat : 이미지 넣기

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+            <Image source={require("./ddong2.png")} style={styles.local_image}></Image>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    text: {
+        color: "red",
+        fontSize: 20,
+        whiteSpace: "nowrap",
+    },
+    local_image: {
+        width: 100,
+        height: 100,
+    },
 });
